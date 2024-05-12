fd5af0df780559b76ecc803dc6e9e764de8cae5c
fede-palazz <fede.palazzi1999@gmail.com>
2024-05-12 21:41:36 +0000

    Added hello function

---
+++ src/kern/include/hello.h
@@ -0,0 +1,10 @@
+#ifndef _HELLO_H_
+#define _HELLO_H_
+
+
+#include "types.h";
+#include "lib.h";
+
+void hello(void);
+
+#endif
---
+++ src/kern/main/hello.c
@@ -0,0 +1,5 @@
+#include "hello.h";
+
+void hello(void) {
+    kprintf("Hello");
+}
